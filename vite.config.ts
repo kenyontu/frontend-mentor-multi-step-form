5befa59a7e4715dc169c6c3b45164af9bf9c72ef
kenyontu <kenyontu@outlook.com>
2022-12-20 13:20:49 +0800

    add base path to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/frontend-mentor-multi-step-form/',
   plugins: [react()],
   resolve: {
     alias: {
